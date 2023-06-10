addf9ff8cd63ea6c81a2e25231dffff1d74810b6
Robert Weeke <robert.weeke@student.hpi.uni-potsdam.de>
2023-06-10 20:54:24 +0200

    remove deprecated startup.sh and edit time on new one

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=8 # -c
 #SBATCH  --gpus=1 # -G
 #SBATCH --mem=16gb # -m
-#SBATCH --time=02:00:00 
+#SBATCH --time=08:00:00 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=robert.weeke@student.hpi.uni-potsdam.de
 
