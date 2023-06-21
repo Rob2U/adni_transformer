ece99bedb98df50757f4f1c305477a617674a740
OliverZim <oliver.zimmermann@student.hpi.de>
2023-06-21 09:44:06 +0200

    shufflenet sweep

--- sweep_config.py
+++ sweep_config.py
@@ -1,6 +1,6 @@
 SWEEP_CONFIG = dict(
-    name="Test sweep",
-    project="test_sweeps",
+    name="exp_shufflenet_1",
+    project="shufflenetv2",
     program="src/train.py",
     method="grid",
     metric=dict(
@@ -12,13 +12,19 @@ SWEEP_CONFIG = dict(
             values=["ShuffleNetV2"],
         ),
         train_fraction=dict(
-            values=[0.5,0.6,0.7],
+            values=[0.7],
         ),
         learning_rate=dict(
-            values=[1e-2, 1e-3, 1e-4, 1e-4],
+            values=[1e-2, 1e-3, 1e-4],
         ),
         batch_size=dict(
             values=[4, 8],
-        ),        
+        ),
+        max_epochs=dict(
+            values=[10],
+        ),
+        benchmark=dict(
+            values=[True],
+        ),
     ),
 )
